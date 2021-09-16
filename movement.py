7a13fdd2254894a6151dafaf17bc4ca2e3a24a98
mkroencke <m.kroencke@outlook.de>
2021-09-16 22:02:58 +0300

    Fixed some movement logic errors; Added logging comments for debugging

--- movement.py
+++ movement.py
@@ -1,11 +1,11 @@
 import math
-import serial
+from serialConnector import SerialConnectorClass
 
 wheelAngle1 = 0
 wheelAngle2 = 120
 wheelAngle3 = 240
 
-ser = serial().start()
+ser = SerialConnectorClass().start()
 
 def calc_wheel_speed(moving_speed, direction, wheel_angle):
     wheel_speed = moving_speed * math.cos(math.radians(direction - wheel_angle))
@@ -14,12 +14,12 @@ def calc_wheel_speed(moving_speed, direction, wheel_angle):
 def omni_move(moving_speed, direction):
     wheel_speeds = [0, 0, 0]
     wheel_speeds[0] = int(calc_wheel_speed(moving_speed, direction, wheelAngle1))
-    wheel_speeds[1] = int(calc_wheel_speed(moving_speed. direction, wheelAngle2))
+    wheel_speeds[1] = int(calc_wheel_speed(moving_speed, direction, wheelAngle2))
     wheel_speeds[2] = - int(calc_wheel_speed(moving_speed, direction, wheelAngle3))
     return wheel_speeds
 
 def set_speeds(wheel_speeds):
-    ser.write_speeds('sd:' + wheel_speeds[0] + ':' + wheel_speeds[1] + ':' + wheel_speeds[2] + '\r\n')
+    ser.write_speeds("sd:" + str(wheel_speeds[0]) + ":" + str(wheel_speeds[1]) + ":" + str(wheel_speeds[2]) + "\r\n")
 
 def move_in_direction(moving_speed, direction):
     wheel_speeds = omni_move(moving_speed, direction)
@@ -38,11 +38,11 @@ def stop():
     set_speeds(wheel_speeds)
 
 def shutdown():
-    ser.write_speeds('d:10\r\n')
+    ser.write_speeds("d:10\r\n")
 
 def close_connection():
+    print("Closing serial port" + ser.port + ".")
     ser.close()
-    print("Connection to serial port was closed.")
 
 def pid(kp, ki, kd, target, position, error_prev, integral_prev, t_prev, t):
     error = target - position
--- serialConnector.py
+++ serialConnector.py
@@ -6,20 +6,21 @@ class SerialConnectorClass:
     def __init__(self):
         # port = '/dev/ttyACM1'
         # port = '/dev/ttyACM0'
-        port = 'COM3'
-        rate = 115200
+        self.port = 'COM3'
+        self.rate = 115200
+        print("Opening connection to port " + self.port + ".")
         self.ser = serial.Serial(
-            port = port,
-            baudrate = rate,
-            bytesize = serial.EIGHTBITS,
-            stopbits = serial.STOPBITS_ONE
+            port=self.port,
+            baudrate=self.rate,
+            bytesize=serial.EIGHTBITS,
+            stopbits=serial.STOPBITS_ONE
         )
-        print("Connection to serial port established.")
 
     def start(self):
         #Thread(target=self.write_speeds, args=()).start()
         return self
 
     def write_speeds(self, speedstr):
+        print("writing " + speedstr + "to port " + self.port)
         self.ser.write(speedstr.encode('utf-8'))
 
