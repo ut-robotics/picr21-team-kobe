47a791ac495bb20e2e45ae9387ec023bd9ca2ded
Uku Ilves <uku.ilves@ut.ee>
2021-09-19 02:18:55 +0300

    added serial communication, now just need to test if the sent command makes to motor move

---
+++ serialcomms.py
@@ -0,0 +1,21 @@
+import time
+import serial
+import struct
+
+ser=serial.Serial('COM3', 115200, timeout=2)
+
+disable_failsafe = 0
+speed1 = 654
+speed2 = 600
+speed3 = 1000
+thrower_speed = 3700
+
+data_size = struct.calcsize('<hhhH')
+while True:
+    data = struct.pack('<hhhHBH', speed1, speed2, speed3, thrower_speed, disable_failsafe, 0xAAAA)
+    ser.write(data)
+
+    received_data = ser.read(data_size)
+    data = struct.unpack('<hhhH', received_data)
+    actual_speed1, actual_speed2, actual_speed_3, feedback_delimiter = data
+    time.sleep(0.25)
