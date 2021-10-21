a3beaf545719c4e7e909ae2db7cf378bfad35ff5
mkroencke <m.kroencke@outlook.de>
2021-10-21 13:46:41 +0300

    Added (tentative) code for handling referee commands

--- socket_client.py
+++ socket_client.py
@@ -1,12 +1,42 @@
-import websockets
 import asyncio
+import websockets
+import json
 
-async def listenServer():
-    url = "ws://localhost:8111"
 
-    async with websockets.connect(url) as websocket:
-        while True:
-            msg = await websocket.recv()
-            print(msg)
+class Client:
 
-asyncio.get_event_loop().run_until_complete(listenServer())
+    def __init__(self):
+        self.run = None
+        self.blueIsTarget = True
+        self.host = "localhost"
+        self.port = 8765
+
+    async def listen_to_server(self):
+        print("Starting client...")
+        uri = "ws://" + self.host + ":" + str(self.port)
+        async with websockets.connect(uri) as websocket:
+            while True:
+                print("Listening to server...")
+                msg = await websocket.recv()
+                cmd = json.loads(msg)
+                print("Received message " + str(cmd))
+                self.process_command(cmd)
+
+    def process_command(self, cmd):
+        print("Processing command " + str(cmd))
+        if cmd["signal"] == "stop":
+            self.run = False
+            self.blueIsTarget = None
+        elif cmd["signal"] == "start":
+            if cmd["basketTarget"] == "blue":
+                self.blueIsTarget = True
+            elif cmd["basketTarget"] == "magenta":
+                self.blueIsTarget = False
+            self.run = True
+
+    def get_current_referee_command(self):
+        return self.run, self.blueIsTarget
+
+
+cl = Client()
+asyncio.get_event_loop().run_until_complete(cl.listen_to_server())
