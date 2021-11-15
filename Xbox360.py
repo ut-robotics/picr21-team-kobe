a9dc0ae6c9642361ea07c4125bac2e08717fa0e3
Uku Ilves <uku.ilves@ut.ee>
2021-11-16 01:38:04 +0200

    gamepad refactor

--- Xbox360.py
+++ Xbox360.py
@@ -9,26 +9,16 @@ def gamepad():
     y=0
     rx=0
     ry=0
-    while True:
-        events = get_gamepad()
-        for event in events:
-            if event.code == "ABS_Y":
-                y = event.state/32768
-                #print(max(min(float(event.state), max_value), min_value))
-            if event.code == "ABS_X":
-                x = event.state/32768
-            if event.code == "ABS_RX":
-                rx = event.state/32768
-            if event.code == "ABS_RY":
-                ry= event.state/32768
-            print(x,y)
-                
-            front_speed = int(y * 10)#math.sqrt(x**2+y**2)
-            side_speed = int(x * 10)
-            rot_speed = int(rx * 10)
-            #return side_speed, front_speed, rot_speed
-            Movement.Move2(side_speed, front_speed, rot_speed, 0)
-            #angle = math.degrees(math.atan2(y,x))
-            #print(speed)#print(normalized)#print(event.ev_type, event.code, event.state)
-
-gamepad()
+    events = get_gamepad()
+    for event in events:
+        if event.code == "ABS_Y":
+            y = event.state/32768
+        if event.code == "ABS_X":
+            x = event.state/32768
+        if event.code == "ABS_RX":
+            rx = event.state/32768
+            
+        front_speed = int(y * 10)
+        side_speed = int(x * 10)
+        rot_speed = int(rx * 10)
+        return side_speed, front_speed, rot_speed
