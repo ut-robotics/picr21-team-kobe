aa76905380af3b117c4471643cc042ce57c5e7a0
Uku <capnarchie@gmail.com>
2021-11-20 11:24:16 +0200

    better list comprehension

--- color_sampler.py
+++ color_sampler.py
@@ -10,15 +10,21 @@ data = np.array(
     
     
 def OrderCheck(data, noise_level, order):
+    #create list in a list everytime element value changes
     grouped = ([list(g) for k, g in groupby(data)])
-    for i in grouped:
-        if len(i) < noise_level:
-            i.clear()
+    grouped = [x.clear() if len(x) < noise_level for x in grouped]
+    #for i in grouped:
+    #    if len(i) < noise_level:
+    #        i.clear()
+    #        # remove empty lists in list
     filtered = [x for x in grouped if x != []]
+    #undo what grouped did
     flat_list = [item for sublist in filtered for item in sublist]
     #print(flat_list)
+    #groups the same values into one e.g "AAAA" -> "A"
     ordered_colors = ([k for k, g in groupby(flat_list)])
     print(ordered_colors)
+    #order must be tuple checks if sequence exists in the list
     order_exists = (order in zip(ordered_colors, ordered_colors[1:],ordered_colors[2:], ordered_colors[3:]))
     return order_exists
 print(OrderCheck(data,2,(1,3,2,3)))
